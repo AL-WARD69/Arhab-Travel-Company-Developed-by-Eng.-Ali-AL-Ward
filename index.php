b1e582027df26ec9f4e1d6a3cfec7e693806874d
Ali AL-Ward <aliabdullahalward@gmail.com>
2024-11-27 23:38:04 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,469 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>ارحب للسفريات والسياحة</title>
+    <link rel="stylesheet" href="fontawesome-5.5/css/all.min.css" />
+    <link rel="stylesheet" href="slick/slick.css">
+    <link rel="stylesheet" href="slick/slick-theme.css">
+    <link rel="stylesheet" href="magnific-popup/magnific-popup.css">
+    <link rel="stylesheet" href="css/bootstrap-rtl.min.css" />
+
+    <link rel="stylesheet" href="css/tooplate-infinite-loop.css" />
+
+-->
+
+  </head>
+  <body dir="rtl">    
+    <!-- Hero section -->
+    <section id="infinite" class="text-white tm-font-big tm-parallax">
+      <!-- Navigation -->
+      <nav style="float:left" class="navbar navbar-expand-md tm-navbar" id="tmNav">              
+        <div class="container">   
+          <div class="tm-next">
+              <a href="#infinite" class="navbar-brand"> شركة أرحب للسفريات</a>
+          </div>             
+            
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars navbar-toggler-icon"></i>
+          </button>
+          <div style="float:left"  class="" id="navbarSupportedContent">
+            <ul style="float:left" class="navbar-nav ml-auto">
+              <li   class="nav-item">
+                  <a style="float:left" class="nav-link tm-nav-link" href="#infinite">الرئيسية</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link tm-nav-link" href="#whatwedo">عن الشركة</a>
+              </li>
+              
+              <li class="nav-item">
+                  <a class="nav-link tm-nav-link" href="#gallery">سياحتك</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link tm-nav-link" href="#contact">اتضل بنا</a>
+              </li>                    
+            </ul>
+          </div>        
+        </div>
+      </nav>
+      
+      <div class="text-center tm-hero-text-container">
+        <div class="tm-hero-text-container-inner">
+            <h2 class="tm-hero-title">أرحب للسفريات والسياحة</h2>
+            <p class="tm-hero-subtitle">
+              تعتبر شركة أرحب الدولية للسفر والسياحة
+              <br>إحدى الشركات الرائدة في مجال إدارة السفر المعتمدة من قبل اتحاد النقل الجوي الدولي المعتمدة
+            </p>
+        </div>        
+      </div>
+
+      <div class="tm-next tm-intro-next">
+        <a href="#whatwedo" class="text-center tm-down-arrow-link">
+          <i class="fas fa-2x fa-arrow-down tm-down-arrow"></i>
+        </a>
+      </div>      
+    </section>
+
+    <section id="whatwedo" class="tm-section-pad-top">
+      
+      <div class="container">
+
+            <div class="row tm-content-box"><!-- first row -->
+                <div class="col-lg-12 col-xl-12">
+                    <div class="tm-intro-text-container">
+                        <h2 class="tm-text-primary mb-4 tm-section-title">من نحن</h2>
+                        <p class="mb-4 tm-intro-text">
+                          أرحب للسفر والسياحة هي شركة متخصصة في تقديم حزم سياحية متكاملة وخدمات سفر مصممة لتلبية احتياجات السياح الباحثين عن تجارب سفر استثنائية حول العالم. مع سنوات من الخبرة في صناعة السياحة، تقدم شركتنا للمسافرين فرصة لاستكشاف أروع الوجهات العالمية براحة وأمان.</p>
+                    </div>
+                </div>
+
+            </div><!-- first row -->
+            
+            <div class="row tm-content-box"><!-- second row -->
+        		<div class="col-lg-1">
+                    <i class="far fa-3x fa-chart-bar text-center tm-icon"></i>
+                </div>
+                <div class="col-lg-5">
+                    <div class="tm-intro-text-container">
+                        <h2 class="tm-text-primary mb-4">هدفنا</h2>
+                        <p class="mb-4 tm-intro-text">
+                          هدفنا هو تحويل أحلامكم السياحية إلى واقع، مع تقديم تجارب سفر مُصممة خصيصاً لتلبية جميع توقعاتكم ورغباتكم. سواء كنتم تخططون لرحلة استجمام في أحضان الطبيعة، أو جولة ثقافية في عمق التاريخ الأوروبي، أو حتى مغامرة مليئة بالأنشطة، فإن "التنظيم للسياحة والسفر" هو شريككم لتحقيق ذلك.</p>
+                    </div>
+                </div>
+                
+                <div class="col-lg-1">
+                    <i class="far fa-3x fa-comment-alt text-center tm-icon"></i>
+                </div>
+                <div class="col-lg-5">
+                    <div class="tm-intro-text-container">
+                        <h2 class="tm-text-primary mb-4">رسالتنا</h2>
+                        <p class="mb-4 tm-intro-text">
+                          تقديم حزم سياحية متكاملة وخدمات سفر مصممة لتلبية احتياجات السياح الباحثين عن تجارب سفر استثنائية حول العالم. مع سنوات من الخبرة في صناعة السياحة، تقدم شركتنا للمسافرين فرصة لاستكشاف أروع الوجهات العالمية براحة وأمان..</p>
+                    </div>
+                </div>
+
+            </div><!-- second row -->
+            
+            <div class="row tm-content-box"><!-- third row -->
+        		<div class="col-lg-1">
+                    <i class="fas fa-3x fa-fingerprint text-center tm-icon"></i>
+                </div>
+                <div class="col-lg-5">
+                    <div class="tm-intro-text-container">
+                        <h2 class="tm-text-primary mb-4">رؤيتنا</h2>
+                        <p class="mb-4 tm-intro-text">
+                          في شركة أرحب للسفر والسياحة، نلتزم بأعلى معايير الجودة في جميع خدماتنا. نحرص على أن تكون كافة تفاصيل رحلاتكم مخطط لها بعناية لضمان تجربة سفر مريحة وممتعة. نحن نستمع إلى تغذيتكم الراجعة لتحسين خدماتنا باستمرار..</p>
+                          
+                         
+                    </div>
+                </div>
+                
+                <div class="col-lg-1">
+                    <i class="fas fa-3x fa-users text-center tm-icon"></i>
+                </div>
+                <div class="col-lg-5">
+                    <div class="tm-intro-text-container">
+                      <h2 class="tm-text-primary mb-4">نجاحنا</h2>
+                        <p class="mb-4 tm-intro-text">
+                          نمارس أعمالنا بمسؤولية تجاه المجتمعات والبيئات التي نعمل فيها. نتبنى مبادئ السياحة المستدامة من خلال تعزيز الممارسات التي تقلل من التأثير البيئي وتدعم الثقافات المحلية. </p>
+                          
+                         
+                    </div>
+                </div>
+
+            </div><!-- third row -->
+
+        </div>
+      
+    </section>
+    
+    
+    
+    <section id="gallery" class="tm-section-pad-top">
+      <div class="container tm-container-gallery">
+        <div class="row">
+          <div class="text-center col-12">
+              <h2 class="tm-text-primary tm-section-title mb-4">سياحتك معنا</h2>
+              <p class="mx-auto tm-section-desc">
+                هناك الكثير من الخدمات التي لن تجدها مجتمعة في أي شركة أخرى فنحن والحمدلله لانترك عملينا دون تحقيق كافة متطلباته وتشمل خدماتنا التالي:              </p>
+          </div>            
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="mx-auto tm-gallery-container">
+                    <div class="grid tm-gallery">
+                      <a href="img/gallery-img-01.jpg">
+                        <figure class="effect-honey tm-gallery-item">
+                          <img src="img/gallery-tn-01.jpg" alt="Image 1" class="img-fluid">
+                          <figcaption>
+                            <h2><i>السياحة الى كندا <span></span></i></h2>
+                          </figcaption>
+                        </figure>
+                      </a>
+                      
+                      <a href="img/gallery-img-02.jpg">
+                        <figure class="effect-honey tm-gallery-item">
+                          <img src="img/gallery-tn-02.jpg" alt="Image 2" class="img-fluid">
+                          <figcaption>
+                            <h2><i>السياحة الى اسبانيا <span></span></i></h2>
+                          </figcaption>
+                        </figure>
+                      </a>
+                      <a href="img/gallery-img-03.jpg">
+                        <figure class="effect-honey tm-gallery-item">
+                          <img src="img/gallery-tn-03.jpg" alt="Image 3" class="img-fluid">
+                          <figcaption>
+                            <h2><i><span>الساحة في السويد</span> </i></h2>
+                          </figcaption>
+                        </figure>
+                      </a>
+                      <a href="img/gallery-img-04.jpg">
+                        <figure class="effect-honey tm-gallery-item">
+                          <img src="img/gallery-tn-04.jpg" alt="Image 4" class="img-fluid">
+                          <figcaption>
+                            <h2><i>السياحة الى روسيا <span></span></i></h2>
+                          </figcaption>
+                        </figure>
+                      </a>
+                      <a href="img/gallery-img-05.jpg">
+                        <figure class="effect-honey tm-gallery-item">
+                          <img src="img/gallery-tn-05.jpg" alt="Image 5" class="img-fluid">
+                          <figcaption>
+                            <h2><i><span>السياحة الى سنغافورة</span> </i></h2>
+                          </figcaption>
+                        </figure>
+                      </a>
+                     
+                    </div>
+                </div>                
+            </div>        
+          </div>
+      </div>
+    </section>
+
+    <!-- Contact -->
+    <section id="contact" class="tm-section-pad-top tm-parallax-2">
+    
+      <div class="container tm-container-contact">
+        
+        <div class="row">
+            
+            <div class="text-center col-12">
+                <h2 class="tm-section-title mb-4">تواصل معنا</h2>
+                <p class="mb-5">
+                  فريق دعم العملاء متاح على مدار الساعة لمساعدة العملاء خلال رحلاتهم والإجابة عن أي استفسارات أو مساعدة في حل أية مشكلات قد تظهر.                </p><br>
+            </div>
+            
+          
+            
+            <div class="col-sm-12 col-md-6">
+
+                <div class="contact-item">
+                  <a rel="nofollow"  class="item-link">
+                    
+                      <i class="far fa-2x fa-comment mr-4"></i>
+                      <h1>  --  </h1>
+                      <span class="mb-0">يمكنكم التواصل معنا عبر احدى الطرق التالية:</span>
+                  </a>              
+                </div>
+                
+                <div class="contact-item">
+                  <a rel="nofollow" href="mailto:mail@company.com" class="item-link">
+                      <i class="far fa-2x fa-envelope mr-4"></i>
+                      <h1>  --  </h1>
+
+                      <span class="mb-0">alwrdly69@gmail.com</span>
+                  </a>              
+                </div>
+                
+                <div class="contact-item">
+                  <a rel="nofollow"  class="item-link">
+                      <i class="fas fa-2x fa-map-marker-alt mr-4"></i>
+                      <h1>  --  </h1>
+
+                      <span class="mb-0">الجمهورية اليمنية - صنعاء - شارع الستين الجنوبي مقابل كاك بنك</span>
+                  </a>              
+                </div>
+                
+                <div class="contact-item">
+                  <a rel="nofollow" href="tel:0100200340" class="item-link">
+                      <i class="fas fa-2x fa-phone-square mr-4"></i>
+                      <h1>  --  </h1>
+
+                      <span class="mb-0">  967774087409 | 967770108882   </span>
+                  </a>              
+                </div>
+                
+                <div class="contact-item">&nbsp;</div>
+            
+            </div>
+            
+            
+        </div><!-- row ending -->
+        
+      </div>
+
+      	<footer class="text-center small tm-footer">
+          <p class="mb-0">
+          جميع الحقوق محفوظة  &copy; 2020 لشركة أرحب
+          
+          . <a rel="nofollow" href="https://www.tooplate.com" title="HTML templates">تم تطوير الموقع بواسطة م:علي الورد</a></p>
+        </footer>
+
+    </section>
+    
+    <script src="js/jquery-1.9.1.min.js"></script>     
+    <script src="slick/slick.min.js"></script>
+    <script src="magnific-popup/jquery.magnific-popup.min.js"></script>
+    <script src="js/easing.min.js"></script>
+    <script src="js/jquery.singlePageNav.min.js"></script>     
+    <script src="js/bootstrap.min.js"></script> 
+    <script>
+
+      function getOffSet(){
+        var _offset = 450;
+        var windowHeight = window.innerHeight;
+
+        if(windowHeight > 500) {
+          _offset = 400;
+        } 
+        if(windowHeight > 680) {
+          _offset = 300
+        }
+        if(windowHeight > 830) {
+          _offset = 210;
+        }
+
+        return _offset;
+      }
+
+      function setParallaxPosition($doc, multiplier, $object){
+        var offset = getOffSet();
+        var from_top = $doc.scrollTop(),
+          bg_css = 'center ' +(multiplier * from_top - offset) + 'px';
+        $object.css({"background-position" : bg_css });
+      }
+
+      // Parallax function
+      // Adapted based on https://codepen.io/roborich/pen/wpAsm        
+      var background_image_parallax = function($object, multiplier, forceSet){
+        multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+        multiplier = 1 - multiplier;
+        var $doc = $(document);
+        // $object.css({"background-attatchment" : "fixed"});
+
+        if(forceSet) {
+          setParallaxPosition($doc, multiplier, $object);
+        } else {
+          $(window).scroll(function(){          
+            setParallaxPosition($doc, multiplier, $object);
+          });
+        }
+      };
+
+      var background_image_parallax_2 = function($object, multiplier){
+        multiplier = typeof multiplier !== 'undefined' ? multiplier : 0.5;
+        multiplier = 1 - multiplier;
+        var $doc = $(document);
+        $object.css({"background-attachment" : "fixed"});
+        
+        $(window).scroll(function(){
+          if($(window).width() > 768) {
+            var firstTop = $object.offset().top,
+                pos = $(window).scrollTop(),
+                yPos = Math.round((multiplier * (firstTop - pos)) - 186);              
+
+            var bg_css = 'center ' + yPos + 'px';
+
+            $object.css({"background-position" : bg_css });
+          } else {
+            $object.css({"background-position" : "center" });
+          }
+        });
+      };
+      
+      $(function(){
+        // Hero Section - Background Parallax
+        background_image_parallax($(".tm-parallax"), 0.30, false);
+        background_image_parallax_2($("#contact"), 0.80);   
+        background_image_parallax_2($("#testimonials"), 0.80);   
+        
+        // Handle window resize
+        window.addEventListener('resize', function(){
+          background_image_parallax($(".tm-parallax"), 0.30, true);
+        }, true);
+
+        // Detect window scroll and update navbar
+        $(window).scroll(function(e){          
+          if($(document).scrollTop() > 120) {
+            $('.tm-navbar').addClass("scroll");
+          } else {
+            $('.tm-navbar').removeClass("scroll");
+          }
+        });
+        
+        // Close mobile menu after click 
+        $('#tmNav a').on('click', function(){
+          $('.navbar-collapse').removeClass('show'); 
+        })
+
+        // Scroll to corresponding section with animation
+        $('#tmNav').singlePageNav({
+          'easing': 'easeInOutExpo',
+          'speed': 600
+        });        
+        
+        // Add smooth scrolling to all links
+        // https://www.w3schools.com/howto/howto_css_smooth_scroll.asp
+        $("a").on('click', function(event) {
+          if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+              scrollTop: $(hash).offset().top
+            }, 600, 'easeInOutExpo', function(){
+              window.location.hash = hash;
+            });
+          } // End if
+        });
+
+        // Pop up
+        $('.tm-gallery').magnificPopup({
+          delegate: 'a',
+          type: 'image',
+          gallery: { enabled: true }
+        });
+
+        $('.tm-testimonials-carousel').slick({
+          dots: true,
+          prevArrow: false,
+          nextArrow: false,
+          infinite: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            }, 
+            {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1
+              }                 
+            }
+          ]
+        });
+
+        // Gallery
+        $('.tm-gallery').slick({
+          dots: true,
+          infinite: false,
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          responsive: [
+          {
+            breakpoint: 1199,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+        });
+      });
+    </script>
+  </body>
+</html>
